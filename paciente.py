745bc909d9ef0bf3ea588c7de85b8e48aa38edbc
Kauã.CMS <227kcvalcante@gmail.com>
2026-06-30 15:57:40 -0300

    Update paciente.py

--- paciente.py
+++ paciente.py
@@ -1,13 +1,13 @@
 #Objeto Cliente o qual saberá calcular seu próprio índice de deteriorização com o tempo
 
-class Pacient:
+class Paciente:
   def __init__(self, nome, probabilidade_gravidade):
     self.nome = nome
     self.p_gravidade_alta = probabilidade_gravidade #<--- Valor vem direto do rede_bayesiana.py
     self.tempo_espera = 0 #<--- Tempo que o paciente já esperou no hosptal antes de ser atendido
 
   def calcular_risco(self): #Esta função irá  calcular o Risco de Deterioração baseado na fórmula linear do enunciado: Risco = P(gravidade_alta) * tempo_esperando
-        return self.p_gravidade_alta * self.tempo_espera
+    return self.p_gravidade_alta * self.tempo_espera
 
-    def __repr__(self):
-        return f"{self.nome} (P: {self.p_gravidade_alta:.2f}, Tempo: {self.tempo_espera}min)"
+  def __repr__(self):
+    return f"{self.nome} (P: {self.p_gravidade_alta:.2f}, Tempo: {self.tempo_espera}min)"
